d6bd8ebb13d440d009be3f59d18d66839e0abd16
SJarno <jarno.saastamoinen@hotmail.com>
2023-02-18 13:09:04 +0200

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+
+import { LoginFormComponent } from './components/forms/login-form/login-form.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginFormComponent,
+
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
